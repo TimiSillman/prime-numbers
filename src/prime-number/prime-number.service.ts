2d75f4645a184015504ae06fe6b8146be45d4112
Timi Sillman <sillmantimi@gmail.com>
2022-05-12 19:52:30 +0300

    Working solution with error handling

--- src/prime-number/prime-number.service.ts
+++ src/prime-number/prime-number.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class PrimeNumberService {
+  // Solution from: https://stackoverflow.com/a/40200710
   isPrime(number: number): boolean {
-    return false
+    for (let i = 2, s = Math.sqrt(number); i <= s; i++) {
+      if (number % i === 0) {
+        return false
+      }
+    }
+    return number > 1
   }
 }
