1894ddbd2cf57315cf3dc08787627ee8c54045c3
Timi Sillman <sillmantimi@gmail.com>
2022-05-12 14:33:56 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrimeNumberService } from './prime-number/prime-number.service';
+import { PrimeNumberController } from './prime-number/prime-number.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, PrimeNumberController],
+  providers: [AppService, PrimeNumberService],
+})
+export class AppModule {}
---
+++ src/prime-number/prime-number.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PrimeNumberService {
+  isPrime(number: number): boolean {
+    return false;
+  }
+}
