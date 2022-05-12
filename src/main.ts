a66419a0d9d3ccb04a90ea6145b00763d40be9cb
Timi Sillman <sillmantimi@gmail.com>
2022-05-12 20:40:47 +0300

    port env variable for azure

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.setGlobalPrefix('api') // all api routes will start with api ex. http//localhost:3000/api/primenumber
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
